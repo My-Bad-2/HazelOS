e84387210effb833c82b455f30d2a1f8d9a7ac9d
Yash Verma <yashverma056@proton.me>
2026-01-02 13:15:12 -0500

    Fix Stacktracer to work with interrupts too

--- kernel/src/arch/x86_64/libs/symbols.c
+++ kernel/src/arch/x86_64/libs/symbols.c
@@ -1,14 +1,23 @@
 #include "libs/symbols.h"
 
+#include <stdint.h>
 #include <stdio.h>
 
 #include "arch.h"
 #include "boot/boot.h"
+#include "cpu/exception.h"
 
 struct stack_frame {
     struct stack_frame* next;  // Saved RBP
     uint64_t rip;              // Return address
 };
+
+extern char __isr_text_start[];
+extern char __isr_text_end[];
+
+static bool is_interrupt_stub(uint64_t rip) {
+    return (rip >= (uint64_t)__isr_text_start && rip < (uint64_t)__isr_text_end);
+}
 
 void dump_stacktrace(void) {
     struct stack_frame* frame;
@@ -18,7 +27,6 @@ void dump_stacktrace(void) {
     arch_write("\n--- CALL TRACE ---\n");
 
     int depth = 0;
-
     while (frame && depth < 20) {
         if ((uint64_t)frame < hhdm_request.response->offset) {
             break;
@@ -32,7 +40,34 @@ void dump_stacktrace(void) {
         snprintf(buf, sizeof(buf), "[0x%lx] %s+0x%lx\n", rip, name, offset);
         arch_write(buf);
 
-        frame = frame->next;
+        if (is_interrupt_stub(rip)) {
+            // `frame` currently points to the RBP pushed by `isr_common_stub`. The stack address
+            // above `frame` contains the `interrupt_trapframe_t`. However, `frame->next` points to
+            // the saved RBP inside that struct. Hence, we calculate the pointer to the trapframe
+            // based on the current rbp.
+            uint64_t stub_rbp           = (uint64_t)frame->next;
+            interrupt_trapframe_t* trap = (interrupt_trapframe_t*)(stub_rbp + 8);
+
+            uint64_t interrupted_rip = trap->rip;
+            uint64_t interrupted_rsp = trap->rsp;
+
+            const char* int_name = resolve_symbol(interrupted_rip, &offset);
+            snprintf(
+                buf,
+                sizeof(buf),
+                "[0x%lx] %s+0x%lx (Interrupted)\n",
+                interrupted_rip,
+                int_name,
+                offset
+            );
+
+            arch_write(buf);
+
+            frame = (struct stack_frame*)trap->rbp;
+        } else {
+            frame = frame->next;
+        }
+
         depth++;
     }
 
